16ac286a1873f903b07f732d2c2af9f1a7293842
olayda <i.ozankaya@gmail.com>
2020-05-03 20:33:04 -0400

    change the location

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -32,7 +32,6 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var mFusedLocationClient: FusedLocationProviderClient
     val PERMISSION_ID = 42
-    var location: Location? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
